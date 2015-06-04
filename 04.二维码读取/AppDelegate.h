fa3b7eb8fb798edcc94dc67f2429ba356a1f0b15
zhouzhe <zhouzhe5391@163.com>
2015-06-05 00:43:24 +0800

    二维码读取第一次提交

---
+++ 04.二维码读取/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  04.二维码读取
+//
+//  Created by apple on 13-11-23.
+//  Copyright (c) 2013年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 04.二维码读取/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  04.二维码读取
+//
+//  Created by apple on 13-11-23.
+//  Copyright (c) 2013年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
